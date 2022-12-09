ea56cd303a8f08d0d7c6935489d917a5b01d22ef
CDConlyred <1746904635@qq.com>
2022-12-09 16:53:21 +0800

    二叉树的前序遍历和最大深度

--- src/java/com/example/coding/Solution.java
+++ src/java/com/example/coding/Solution.java
@@ -149,5 +149,31 @@ public class Solution {
         return ans;
     }
 
+    /**
+     * leetcode 144.二叉树的前序遍历
+     */
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        preOrder(root, ans);
+        return ans;
+    }
+    private void preOrder(TreeNode root, List<Integer> res) {
+        if (root == null) return;
+        res.add(root.val);
+        preOrder(root.left, res);
+        preOrder(root.right, res);
+    }
+
+    /**
+     * leetcode 104.二叉树的最大深度
+     */
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left, right) + 1;
+    }
+
+
 
 }
