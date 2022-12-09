33ac7b5a15a4a8070e5ced9f46f624e9b05137b1
CDConlyred <1746904635@qq.com>
2022-12-09 17:23:04 +0800

    二叉树的中序遍历

--- src/java/com/example/coding/Solution.java
+++ src/java/com/example/coding/Solution.java
@@ -191,5 +191,22 @@ public class Solution {
         return ans;
     }
 
+    /**
+     * leetcode 94.二叉树的中序遍历
+     * @param root
+     * @return
+     */
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        inOrder(root, res);
+        return res;
+    }
+    private void inOrder(TreeNode root, List<Integer> res) {
+        if (root == null) return;
+        inOrder(root.left, res);
+        res.add(root.val);
+        inOrder(root.right, res);
+    }
+
 
 }
