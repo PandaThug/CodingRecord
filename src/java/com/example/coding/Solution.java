bc4f6e534e69d64aa49b826b4f6ce9623a55d9be
CDConlyred <1746904635@qq.com>
2022-12-11 02:36:05 +0800

    leetcode 1827.最少操作使数组递增

--- src/java/com/example/coding/Solution.java
+++ src/java/com/example/coding/Solution.java
@@ -28,6 +28,7 @@ public class Solution {
         }
     }
 
+
     /**
      * leetcode 143.重排链表
      */
@@ -207,6 +208,22 @@ public class Solution {
         res.add(root.val);
         inOrder(root.right, res);
     }
+
+    /**
+     * leetcode 1827.最少操作使数组递增
+     */
+    public int minOperations(int[] nums) {
+        if (nums.length == 1) return 0;
+        int res = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i + 1] <= nums[i]) {
+                res += (nums[i] - nums[i + 1] + 1);
+                nums[i + 1] = nums[i] + 1;
+            }
+        }
+        return res;
+    }
+
 
 
 }
