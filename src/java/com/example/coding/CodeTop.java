6733ee58f211f98eb859514abcc970dc345079e2
CDConlyred <1746904635@qq.com>
2022-12-14 23:22:04 +0800

    leetcode 200.岛屿数量

--- src/java/com/example/coding/CodeTop.java
+++ src/java/com/example/coding/CodeTop.java
@@ -250,4 +250,33 @@ public class CodeTop {
         return ans;
     }
 
+    /**
+     * leetcode 200.岛屿数量
+     */
+    public int numIsIands(char[][] grid) {
+        if (grid == null || grid.length == 0) return 0;
+        int nr = grid.length, nc = grid[0].length;
+        int cnt = 0;
+        for (int r = 0; r < nr; r++) {
+            for (int c = 0; c < nc; c++) {
+                if (grid[r][c] == '1') {
+                    ++cnt;
+                    dfs(grid, r, c);
+                }
+            }
+        }
+        return cnt;
+    }
+    private void dfs(char[][] grid, int r, int c) {
+        int nr = grid.length, nc = grid[0].length;
+        if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
+            return;
+        }
+        grid[r][c] = '0';
+        dfs(grid, r - 1, c);
+        dfs(grid, r + 1, c);
+        dfs(grid, r, c - 1);
+        dfs(grid, r, c + 1);
+    }
+
 }
