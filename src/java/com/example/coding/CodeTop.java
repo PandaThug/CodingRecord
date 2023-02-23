5b6ed7d7236af407a4c0b4d25ed0b65e4ca4fe33
CDConlyred <1746904635@qq.com>
2023-02-23 18:42:53 +0800

    1

--- src/java/com/example/coding/CodeTop.java
+++ src/java/com/example/coding/CodeTop.java
@@ -490,4 +490,27 @@ public class CodeTop {
         return head;
     }
 
+    /**
+     * 56. 合并区间
+     * @param intervals
+     * @return
+     */
+    public int[][] merge(int[][] intervals) {
+        if (intervals.length == 0) {
+            return new int[0][2];
+        }
+        Arrays.sort(intervals, Comparator.comparingInt(interval -> interval[0]));
+        List<int[]> merged = new ArrayList<>();
+        for (int[] interval : intervals) {
+            int left = interval[0];
+            int right = interval[1];
+            if (merged.isEmpty() || merged.get(merged.size() - 1)[1] < left) {
+                merged.add(new int[]{left, right});
+            } else {
+                merged.get(merged.size() - 1)[1] = Math.max(merged.get(merged.size() - 1)[1], right);
+            }
+        }
+        return merged.toArray(new int[merged.size()][]);
+    }
+
 }
